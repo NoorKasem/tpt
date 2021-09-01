26d2b33d82a0ae17b5eaf4720bd5047a820aacf5
NoorKasem <77166845+NoorKasem@users.noreply.github.com>
2021-09-01 15:52:46 +0300

    /

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,13 +1,15 @@
 <template>
-  <v-app-bar dark  fixed>
+  <v-app-bar dark fixed>
     <v-img contain width="50" height="50" src="~@/assets/logo.png"></v-img>
+    <v-btn x-large icon>
+      <v-icon color="#ffc400">mdi-lightbulb-on</v-icon>
+    </v-btn>
 
     <v-spacer></v-spacer>
 
-    <v-btn  v-scroll-to="'#services'" class="btn" text x-large>Services</v-btn>
+    <v-btn v-scroll-to="'#services'" class="btn" text x-large>Services</v-btn>
     <v-btn v-scroll-to="'#d2'" class="btn" text x-large>Why Choose Us</v-btn>
-    <v-btn  v-scroll-to="'#footer'" class="btn" text x-large>Contact</v-btn>
-   
+    <v-btn v-scroll-to="'#footer'" class="btn" text x-large>Contact</v-btn>
 
     <v-spacer></v-spacer>
     <v-spacer></v-spacer>
@@ -18,6 +20,7 @@
     <v-btn class="img-nav" icon>
       <v-icon color="red">mdi-gmail</v-icon>
     </v-btn>
+    
   </v-app-bar>
 </template>
 
@@ -30,7 +33,7 @@
 }
 .btn:hover {
   border-bottom: 4px solid #ffc400;
-  border-top:  4px solid #ffc400 ;
+  border-top: 4px solid #ffc400;
 }
 .img-nav {
   border-radius: 50%;
@@ -38,6 +41,6 @@
 }
 .img-nav:hover {
   transform: scale(1.1);
-  cursor:pointer;  
+  cursor: pointer;
 }
 </style>
